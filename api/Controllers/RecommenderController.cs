149a529377fdbb18ef34a2769b6d05b60f650d52
filippopellizzari <pellipippo@hotmail.it>
2016-11-22 11:39:10 +0100

    MOV-26 reset branch #time 15m

---
+++ api/Controllers/RecommenderController.cs
@@ -0,0 +1,33 @@
+﻿using api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace api.Controllers
+{
+    [RoutePrefix("api/Recommender")]
+    public class RecommenderController : ApiController
+    {
+        //will be the query result of recommended movies
+        RecommendedMovie[] movies = new RecommendedMovie[]
+        {
+            new RecommendedMovie { Id = 1, Name = "Forrest Gump", Category = "Comedy"},
+            new RecommendedMovie { Id = 2, Name = "Titanic", Category = "Drama"},
+            new RecommendedMovie { Id = 3, Name = "Avatar", Category = "Sci-Fi"},
+            new RecommendedMovie { Id = 4, Name = "The Avengers", Category = "Action"},
+            new RecommendedMovie { Id = 5, Name = "The Matrix", Category = "Sci-Fi"},
+        };
+
+        [Route("GetAllMovies")]
+        public IEnumerable<RecommendedMovie> GetAllRecommendedMovies()
+        {
+            return movies;
+        }
+
+
+    }
+}
