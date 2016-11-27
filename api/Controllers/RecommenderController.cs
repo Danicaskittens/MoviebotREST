925e61eac7829ffb0ee0d71e837b9aaa8955d8bd
achelius <rompamail2@gmail.com>
2016-11-27 18:36:09 +0100

    MOV-38 recommender Controller fixes

--- api/Controllers/RecommenderController.cs
+++ api/Controllers/RecommenderController.cs
@@ -1,4 +1,5 @@
-﻿using api.Models;
+﻿using api.Adapters;
+using api.Models;
 using api.Models.Data;
 using api.Models.Output;
 using api.Models.OutputModels;
@@ -11,55 +12,23 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 
 namespace api.Controllers
-{   
+{
+    /// <summary>
+    /// Returns recommendations for the user 
+    /// </summary>
     [RoutePrefix("api/v1/Recommender")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class RecommenderController : ApiController
     {
-        private static List<Movie> getRecommendedMovies()
-        {
-            Movie movie1 = new Movie()
-            {
-                Title = "Forrest Gump",
-                imdbID = "tt0838881",
-                Genre = "Comedy"
-            };
-            Movie movie2 = new Movie()
-            {
-                Title = "Titanic",
-                imdbID = "tt2393120",
-                Genre = "Drama"
-            };
-            Movie movie3 = new Movie()
-            {
-                Title = "Avatar",
-                imdbID = "tt1032612",
-                Genre = "Sci-Fi"
-            };
-            Movie movie4 = new Movie()
-            {
-                Title = "The Avengers",
-                imdbID = "tt1928374",
-                Genre = "Action"
-            };
-            Movie movie5 = new Movie()
-            {
-                Title = "The Matrix",
-                imdbID = "tt0099362",
-                Genre = "Sci-Fi"
-            };
 
-            return new List<Movie>() { movie1, movie2, movie3 };
-        }
-      
- 
         /// <summary>
         /// return all the recommended movies for the user homepage
         /// </summary>
-        [Route("GetAllRecommendedMovies")]
+        [Route("movies")]
         [HttpGet]
         public JsonApiOutput<IEnumerable<MovieOutputModel>> GetAllRecommendedMovies()
         {
-            return new JsonApiOutput<IEnumerable<MovieOutputModel>>(getRecommendedMovies().
+            return new JsonApiOutput<IEnumerable<MovieOutputModel>>(DatabaseAdapter.queryRecommendedMoviesForUser("placeholder").
                 Select<Movie, MovieOutputModel>(i => new MovieOutputModel(i)));
         }
 
