aab51a9dac6d89cbfddb19e9f193c002dba9d40b
achelius <rompamail2@gmail.com>
2016-11-29 16:58:47 +0100

    MOV-68 added some info of the dummy movies

--- api/Adapters/DatabaseAdapter.cs
+++ api/Adapters/DatabaseAdapter.cs
@@ -70,35 +70,45 @@ namespace api.Adapters
                 Title = "Forrest Gump",
                 imdbID = "tt0109830",
                 Genre = "Comedy",
-                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg"
+                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg",
+                Plot = "Forrest Gump, while not intelligent, has accidentally been present at many historic moments, but his true love, Jenny Curran, eludes him.",
+                Runtime = "134min"
             };
             Movie movie2 = new Movie()
             {
                 Title = "Titanic",
                 imdbID = "tt0120338",
                 Genre = "Drama",
-                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BZDNiMjE0NDgtZWRhNC00YTlhLTk2ZjItZTQzNTU2NjAzNWNkXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
+                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BZDNiMjE0NDgtZWRhNC00YTlhLTk2ZjItZTQzNTU2NjAzNWNkXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
+                Plot = "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
+                Runtime = "134min"
             };
             Movie movie3 = new Movie()
             {
                 Title = "Avatar",
                 imdbID = "tt0499549",
                 Genre = "Sci-Fi",
-                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg"
+                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+                Plot = "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+                Runtime = "134min"
             };
             Movie movie4 = new Movie()
             {
                 Title = "The Avengers",
                 imdbID = "tt0848228",
                 Genre = "Action",
-                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SX300.jpg"
+                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SX300.jpg",
+                Plot = "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity.",
+                Runtime = "134min"
             };
             Movie movie5 = new Movie()
             {
                 Title = "The Matrix",
                 imdbID = "tt0133093",
                 Genre = "Sci-Fi",
-                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMDMyMmQ5YzgtYWMxOC00OTU0LWIwZjEtZWUwYTY5MjVkZjhhXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg"
+                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMDMyMmQ5YzgtYWMxOC00OTU0LWIwZjEtZWUwYTY5MjVkZjhhXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg",
+                Plot = "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+                Runtime ="134min"
             };
 
             return new List<Movie>() { movie1, movie2, movie3, movie4, movie5 };
