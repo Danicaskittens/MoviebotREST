b29efd80345ded8b942652e26fd93751f8d3fdc2
achelius <rompamail2@gmail.com>
2016-12-07 01:20:21 +0100

    MOV-159 documentation on MovieBotContext #time 2m

--- api/DAL/MovieBotContext.cs
+++ api/DAL/MovieBotContext.cs
@@ -6,14 +6,28 @@ using System.Web;
 
 namespace api.DAL
 {
+    /// <summary>
+    /// The Entity framework context for everything concerning the moviebot database
+    /// </summary>
     public class MovieBotContext : DbContext
     {
+        /// <summary>
+        /// Creates a new Moviebotcontext using the MovieBotDb database on default initializator connection (see web.config for details)
+        /// </summary>
         public MovieBotContext():base("MovieBotDb"){
             Database.SetInitializer<MovieBotContext>(new MovieBotInitializer());
         }
+        /// <summary>
+        /// List of Cinemas actually present on the database
+        /// </summary>
         public DbSet<Cinema> Cinemas { get; set; }
+        /// <summary>
+        /// List of the movies actually present on the database
+        /// </summary>
         public DbSet<Movie> Movies { get; set; }
-
+        /// <summary>
+        /// List of projections present on the database
+        /// </summary>
         public DbSet<Projection> Projections { get; set; }
     }
 }
