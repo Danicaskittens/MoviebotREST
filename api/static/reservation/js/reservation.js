75b45bdea9f9c602d1246fe19825841bcb7b0190
achelius <rompamail2@gmail.com>
2017-01-09 22:27:20 +0100

    MOV-194 last minute fixes for reservation page for the bot  #time 10m

--- api/static/reservation/js/reservation.js
+++ api/static/reservation/js/reservation.js
@@ -4,20 +4,25 @@ function ready() {
     $(".button").on("click", function () {
         $(this).css("background-color", "green");
     })
-
-    var toAdd=QueryString.cinemaName;
+    var toAdd = decodeURI(QueryString.cinemaName);
+    toAdd = toAdd.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); })
     $("#cinema").append(toAdd);
 
-    var toAdd=QueryString.movieName;
+    var toAdd = decodeURI(QueryString.movieName);
+    toAdd = toAdd.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); })
     $("#movie").append(toAdd);
 
-    var toAdd=QueryString.date;
+    var toAdd = QueryString.date;
+    var toAdd = decodeURI(QueryString.date);
+    toAdd = toAdd.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); })
     $("#date").append(toAdd);
 
-    var toAdd=QueryString.time;
+    var toAdd = decodeURI(QueryString.time);
+    toAdd = toAdd.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); })
     $("#time").append(toAdd);
 
-    var toAdd=QueryString.freeSeats;
+    var toAdd = decodeURI(QueryString.freeSeats);
+    toAdd = toAdd.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); })
     $("#freeSeats").append(toAdd);
 
     $("#confirm-button").on("click", function(){
