3283c44a47b2eb4ef672d6a1d6d3c37ed88d5169
filippopellizzari <pellipippo@hotmail.it>
2017-01-16 23:47:29 +0100

    MOV-256 set up some unit tests #time 1h

---
+++ api.Tests/Controllers/CinemasControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using api.Controllers;
+using api.Models.OutputModels;
+using System.Collections.Generic;
+
+namespace api.Tests.Controllers
+{
+    [TestClass]
+    public class CinemasControllerTest
+    {
+        [TestInitialize]
+        public void initialize()
+        {
+            Utils.setMockContext();
+        }
+
+        [TestMethod]
+        public void getCinemasByNameTest()
+        {
+            CinemasController controller = new CinemasController();
+            var result = controller.GetCinemasByName("targa") as System.Web.Http.Results.OkNegotiatedContentResult<JsonApiOutput<IEnumerable<CinemaOutputModel>>>;
+            IEnumerable<CinemaOutputModel> cinemasList = result.Content.Data;
+            foreach (var item in cinemasList)
+            {
+                if (item.Name.Contains("targa"))
+                {
+                    Assert.IsTrue(true);
+                    return;
+                }
+            }
+            Assert.Fail();
+        }
+
+    }
+}
---
+++ api.Tests/Controllers/ReservationsControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using api.Controllers;
+using api.Models.OutputModels;
+
+namespace api.Tests.Controllers
+{
+    [TestClass]
+    public class ReservationsControllerTest
+    {
+
+        [TestInitialize]
+        public void initialize()
+        {
+            Utils.setMockContext();
+        }
+
+        [TestMethod]
+        public void freeSeatsReservedTest()
+        {
+            ReservationsController controller = new ReservationsController();
+            //TODO mock a reservation process and check the consistency of seats
+            Assert.IsTrue(true);
+        }
+    }
+}
