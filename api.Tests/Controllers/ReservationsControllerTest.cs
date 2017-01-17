ef939c2457c40c0466f42987e000fc700e24404b
filippopellizzari <pellipippo@hotmail.it>
2017-01-17 23:10:59 +0100

    MOV-256 mock context #time 1h

--- api.Tests/Controllers/ReservationsControllerTest.cs
+++ api.Tests/Controllers/ReservationsControllerTest.cs
@@ -2,23 +2,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using api.Controllers;
 using api.Models.OutputModels;
+using Moq;
+using System.Data.Entity;
+using api.DAL;
+using System.Security.Principal;
+using System.Web.Mvc;
+using System.Web;
 
 namespace api.Tests.Controllers
 {
     [TestClass]
     public class ReservationsControllerTest
     {
-
         [TestInitialize]
         public void initialize()
         {
             Utils.setMockContext();
+            ReservationsController controller = new ReservationsController();
+            //controller.AddNewReservation(23);
         }
 
         [TestMethod]
         public void freeSeatsReservedTest()
         {
-            ReservationsController controller = new ReservationsController();
             //TODO mock a reservation process and check the consistency of seats
             Assert.IsTrue(true);
         }
